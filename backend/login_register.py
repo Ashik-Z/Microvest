67615926199498454095d68abeed94d06ed23ed6
Ashik-Z <rmahfuzurashik24@gmail.com>
2025-10-01 15:43:14 +0600

    updated password security to hashing

--- backend/login_register.py
+++ backend/login_register.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -24,14 +25,15 @@ def login():
         username = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s", (username, password))
+        cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
 
         account = cursor.fetchone()
-        if account:
+        if account and check_password_hash(account['password'], password):
             session['loggedin'] = True
             session['id'] = account['user_id']
             session['username'] = account['username']
             return render_template('index.html', msg = "Logged in successfully!")
+
         else:
             msg = 'Incorrect username/password!'
     return render_template('login.html', msg = msg)
@@ -54,22 +56,31 @@ def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
-        password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
 
+        cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
         account = cursor.fetchone()
+
+        cursor.execute("SELECT * FROM user WHERE email = %s", (email,))
+        email_exists = cursor.fetchone()
+
         if account:
-            msg = 'Account already exists!'
+            msg = 'Username already exists!'
+        elif email_exists:
+            msg = 'Email already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not username or not request.form["password"] or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute("INSERT INTO user (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
+            raw_password = request.form['password']
+            hashed_password = generate_password_hash(raw_password, method='pbkdf2:sha256')
+            cursor.execute("INSERT INTO user (username, password, email) VALUES (%s, %s, %s)",
+                           (username, hashed_password, email)
+                           )
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     return render_template('register.html', msg = msg)
